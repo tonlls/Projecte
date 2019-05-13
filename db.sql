1fbd41e7fc5ae7538053e699833ec7100bd3f103
tonlls <tonlls1999@gmail.com>
2019-05-14 01:04:44 +0200

    db model improved and errors solved

--- db.sql
+++ db.sql
@@ -1,3 +1,6 @@
+drop database if exists port_aventura;
+create database port_aventura;
+use port_aventura;
 create table preus(
 	parc1_id int REFERENCES parc(id),
 	parc2_id int REFERENCES parc(id),
@@ -11,8 +14,8 @@ create table entrada(
 	id int PRIMARY KEY AUTO_INCREMENT,
 	data date,
 	dies_validesa int,
-	preu float
-	propietari_id int REFERENCES client(id)
+	preu float,
+	propietari_id int REFERENCES client(id),
 	categoria_id int REFERENCES categoria(id)
 );
 create table entrada_parc(
@@ -22,17 +25,17 @@ create table entrada_parc(
 );
 create table categoria_entrada(
 	id int PRIMARY KEY,
-	nom varchar UNIQUE
+	nom varchar(30) UNIQUE
 );
 create table parc(
 	id int PRIMARY KEY,
-	nom varchar,
-	url_foto varchar
-)
+	nom varchar(30),
+	url_foto varchar(50)
+);
 create table zona(
 	id int,
 	parc_id int,
-	nom varchar,
+	nom varchar(30),
 	PRIMARY KEY(id,parc_id)
 );
 create table atraccio(
@@ -41,35 +44,35 @@ create table atraccio(
 	--incidencia_actual_id int REFERENCES incidencia(id),
 	estat_actual_id int REFERENCES estat_operatiu(id),
 	capacitat_maxima_ronda int,
-	descripcioHTML varchar,
-	nom varchar,
+	descripcioHTML varchar(300),
+	nom varchar(30),
 	temps_per_ronda int,
-	url_foto varchar,
+	url_foto varchar(50),
 	clients_cua int,
 	alçada_minima_acompanyat int,
 	alçada_minima int 
 );
 create table tipus_acces(
 	id int PRIMARY KEY,
-	nom varchar UNIQUE
+	nom varchar(30) UNIQUE
 );
 create table estat_operatiu(
 	id int PRIMARY KEY,
-	nom varchar UNIQUE
+	nom varchar(30) UNIQUE
 );
 create table incidencia(
-	id int PRIMARY KEY,
+	id int,
 	atraccio_id int REFERENCES atraccio(id),
 	estat_operatiu_id int REFERENCES estat_operatiu(id),
 	data_fi date,
 	data_inici date,
-	missatge_estat varchar,
+	missatge_estat varchar(50),
 	data_fi_prevista date,
 	PRIMARY KEY(id,atraccio_id)
 );
 create table tipus_passi_expres(
 	id int PRIMARY KEY,
-	nom varchar,
+	nom varchar(30) UNIQUE,
 	preu_dia float
 );
 create table passi_express(
@@ -85,9 +88,9 @@ create table tipus_acces_atraccio(
 create table client(
 	id int PRIMARY KEY,
 	nif varchar(10),
-	nom varchar,
-	cognom1 varchar,
-	cognom2 varchar	
+	nom varchar(30),
+	cognom1 varchar(30),
+	cognom2 varchar(30)
 );
 
 --una atracció pot tenir mes d'una incidencia oberta??, si pot incidencia actual es incoherent
