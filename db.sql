5fe6aaf5735f2b3903f30778a3dd1bb5f0b670ff
Ton <47422572+tonlls@users.noreply.github.com>
2019-05-14 11:39:30 +0200

    db struct improvements

--- db.sql
+++ db.sql
@@ -1,69 +1,81 @@
 DROP database IF EXISTS port_aventura;
 CREATE database port_aventura;
 USE port_aventura;
--- enums----------------------------------
+
 CREATE TABLE categoria_entrada(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nom VARCHAR(30) UNIQUE
+	id INT AUTO_INCREMENT,
+	nom VARCHAR(30),
+	PRIMARY KEY (id),
+	UNIQUE (nom)
 );
 CREATE TABLE tipus_acces(
-	id INT PRIMARY KEY,
-	nom VARCHAR(30) NOT NULL UNIQUE
+	id INT,
+	nom VARCHAR(30) NOT NULL,
+	PRIMARY KEY (id),
+	UNIQUE (nom)
 );
 CREATE TABLE estat_operatiu(
-	id INT PRIMARY KEY,
-	nom VARCHAR(30) NOT NULL UNIQUE
+	id INT,
+	nom VARCHAR(30) NOT NULL,
+	PRIMARY KEY (id),
+	UNIQUE (nom)
 );
--- -----------------------------------------
 CREATE TABLE preu(
-	id INT PRIMARY KEY,
+	id INT,
 	dies INT NOT NULL,
 	preu_adult INT,
 	preu_nen_senior INT,
-	preu_discapacitat INT
-);
-/*
-CREATE TABLE preus(
-	parc1_id INT REFERENCES parc(id),
-	parc2_id INT REFERENCES parc(id),
-	parc3_id INT REFERENCES parc(id),
-	num_dies INT,
-	preu_adult INT,
-	preu_nen_senior INT,
-	preu_discapacitat INT
-);
-*/
-CREATE TABLE entrada(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	data DATE,
-	dies_validesa INT,
-	preu float,
-	propietari_id INT REFERENCES client(id),
-	categoria_id INT REFERENCES categoria(id)
+	preu_discapacitat INT,
+	PRIMARY KEY (id)
 );
 CREATE TABLE client(
-	id INT PRIMARY KEY,
+	id INT,
 	nif VARCHAR(10) NOT NULL,
 	nom VARCHAR(30),
 	cognom1 VARCHAR(30),
-	cognom2 VARCHAR(30)
+	cognom2 VARCHAR(30),
+	PRIMARY KEY (id)
 );
 CREATE TABLE parc(
-	id INT PRIMARY KEY,
+	id INT,
 	nom VARCHAR(30) NOT NULL,
-	url_foto VARCHAR(50)
+	url_foto VARCHAR(50),
+	PRIMARY KEY (id)
+);
+CREATE TABLE tipus_passi_expres(
+	id INT,
+	nom VARCHAR(30),
+	preu_dia FLOAT,
+	PRIMARY KEY (id),
+	UNIQUE (nom)
+);
+
+
+CREATE TABLE entrada(
+	id INT AUTO_INCREMENT,
+	data DATE,
+	dies_validesa INT,
+	preu float,
+	client_id INT,
+	categoria_id INT,
+	preu_id INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (client_id) REFERENCES client(id),
+	FOREIGN KEY (categoria_id) REFERENCES categoria_entrada(id),
+	FOREIGN KEY (preu_id) REFERENCES preu(id)
 );
 CREATE TABLE zona(
 	id INT,
-	parc_id INT REFERENCES parc(id),
+	parc_id INT,
 	nom VARCHAR(30) NOT NULL,
-	PRIMARY KEY(id,parc_id)
+	PRIMARY KEY (id,parc_id),
+	FOREIGN KEY (parc_id) REFERENCES parc(id)
 );
 CREATE TABLE atraccio(
-	id INT PRIMARY KEY,
-	zona_id INT REFERENCES zona(id),
+	id INT,
+	zona_id INT,
 	-- incidencia_actual_id INT REFERENCES incidencia(id),
-	estat_actual_id INT REFERENCES estat_operatiu(id),
+	estat_actual_id INT,
 	capacitat_maxima_ronda INT,
 	descripcioHTML VARCHAR(300),
 	nom VARCHAR(30) NOT NULL,
@@ -71,43 +83,53 @@ CREATE TABLE atraccio(
 	url_foto VARCHAR(50),
 	clients_cua INT,
 	alçada_minima_acompanyat INT,
-	alçada_minima INT 
+	alçada_minima INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (zona_id) REFERENCES zona(id),
+	FOREIGN KEY (estat_actual_id) REFERENCES estat_operatiu(id)
 );
 CREATE TABLE incidencia(
 	id INT,
-	atraccio_id INT REFERENCES atraccio(id),
-	estat_operatiu_id INT REFERENCES estat_operatiu(id),
+	atraccio_id INT,
+	estat_operatiu_id INT,
 	data_fi DATE,
 	data_inici DATE,
 	missatge_estat VARCHAR(50),
 	data_fi_prevista DATE,
-	PRIMARY KEY(id,atraccio_id)
+	PRIMARY KEY(id,atraccio_id),
+	FOREIGN KEY (atraccio_id) REFERENCES atraccio(id),
+	FOREIGN KEY (estat_operatiu_id) REFERENCES estat_operatiu(id)
 );
-CREATE TABLE passi_express(
-	id INT PRIMARY KEY,
-	client INT,
-	data DATE
-);
--- -------------------------
+
 CREATE TABLE preu_parc(
-	preu_id INT REFERENCES preu(id),
-	parc_id INT REFERENCES parc(id),
-	PRIMARY KEY(preu_id,parc_id)
+	preu_id INT,
+	parc_id INT,
+	PRIMARY KEY(preu_id,parc_id),
+	FOREIGN KEY (preu_id) REFERENCES preu(id),
+	FOREIGN KEY (parc_id) REFERENCES parc(id)
 );
 CREATE TABLE entrada_parc(
-	entrada_id INT REFERENCES entrada(id),
-	parc_id INT REFERENCES parc(id),
-	PRIMARY KEY(entrada_id,parc_id)
-);
-CREATE TABLE tipus_passi_expres(
-	id INT PRIMARY KEY,
-	nom VARCHAR(30) UNIQUE,
-	preu_dia FLOAT
+	entrada_id INT,
+	parc_id INT,
+	PRIMARY KEY(entrada_id,parc_id),
+	FOREIGN KEY (entrada_id) REFERENCES entrada(id),
+	FOREIGN KEY (parc_id) REFERENCES parc(id)
 );
 CREATE TABLE tipus_acces_atraccio(
-	tipus_pasi_id INT REFERENCES tipus_passi_expres(id),
-	atraccio_id INT REFERENCES atraccio(id),
-	PRIMARY KEY(tipus_pasi_id,atraccio_id)
+	tipus_passi_id INT,
+	atraccio_id INT,
+	PRIMARY KEY (tipus_passi_id,atraccio_id),
+	FOREIGN KEY (tipus_passi_id) REFERENCES tipus_passi_expres(id),
+	FOREIGN KEY (atraccio_id) REFERENCES atraccio(id)
+);
+CREATE TABLE passi_express(
+	id INT,
+	client_id INT,
+	tipus_id INT,
+	data DATE,
+	PRIMARY KEY (id),
+	FOREIGN KEY (client_id) REFERENCES client(id),
+	FOREIGN KEY (tipus_id) REFERENCES tipus_passi_expres(id)
 );
 
 -- una atracció pot tenir mes d'una incidencia oberta??, si pot incidencia actual es incoherent
@@ -123,3 +145,14 @@ CREATE TABLE tipus_acces_atraccio(
 -- falten taules? usuaris/passwords? cua atraccio?
 
 commit;
+/*
+CREATE TABLE preus(
+	parc1_id INT REFERENCES parc(id),
+	parc2_id INT REFERENCES parc(id),
+	parc3_id INT REFERENCES parc(id),
+	num_dies INT,
+	preu_adult INT,
+	preu_nen_senior INT,
+	preu_discapacitat INT
+);
+*/
